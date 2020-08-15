624e6d6a7dbdcc71d304fc73f87827f157958837
Oleksandr Kozachuk <oleksandr.kozachuk@exasol.com>
2020-08-15 11:13:04 +0200

    Support quantity and purchase sets

--- kooking_purchases.py
+++ kooking_purchases.py
@@ -1,7 +1,7 @@
 from utils import *
 from pprint import pprint as pp
 from datetime import datetime, timedelta
-import requests, json, sys, pytz, time, os, hashlib
+import requests, json, sys, pytz, time, os, hashlib, string
 
 products_to_display = {'b845d390-a4f8-11ea-8f9b-5ca8cba5baac',
                        'f13f3660-19dd-11ea-b6e8-50ad43873b88',
@@ -15,35 +15,52 @@ access_token = None
 access_token_usage = 100
 timezone = pytz.timezone('Europe/Oslo')
 
-def get_purchases():
+def get_purchases(offset = 0):
     global access_token_usage, access_token
     if access_token_usage >= 100:
         access_token = get_access_token()
         access_token_usage = 0
     else: access_token_usage += 1
-    start_date = datetime.now().strftime('%Y-%m-%d')
+    start_date = (datetime.now() - timedelta(days = offset)).strftime('%Y-%m-%d')
     res = requests.get('https://purchase.izettle.com/purchases/v2',
                        headers = {'Authorization': 'Bearer %s' % access_token},
                        params = {'startDate': start_date})
     return json.loads(res.text)['purchases']
 
 def kooking_purchases(purchases_list):
-    ret = []
+    ret = []; setnum = 0;
     for purchase in purchases_list:
+        found = False; prodnum = 0; prodquant = 0
+        for product in purchase['products']:
+            if 'productUuid' in product and product['productUuid'] in products_to_display:
+                found = True
+                prodquant += int(product['quantity'])
+        if not found: continue
+        setnum += 1
+        setletter = string.ascii_letters[setnum % len(string.ascii_letters)]
         for product in purchase['products']:
             if 'productUuid' in product and product['productUuid'] in products_to_display:
                 timestamp = datetime.strptime(purchase['timestamp'], '%Y-%m-%dT%H:%M:%S.%f%z')
                 ts = timestamp.astimezone(timezone).isoformat()
-                ret.append({'ts': ts,
-                            'purchase': purchase['purchaseUUID'],
-                            'product': product['productUuid'],
-                            'name': product['name']})
+                quantity = int(product['quantity'])
+                for num in range(quantity):
+                    prodnum += 1
+                    ret.append({'ts': ts,
+                                'purchase': purchase['purchaseUUID'],
+                                'product': product['productUuid'],
+                                'setnum': setnum,
+                                'setletter': setletter,
+                                'num': num,
+                                'quantity': quantity,
+                                'prodnum': prodnum,
+                                'prodquant': prodquant,
+                                'name': product['name']})
     return ret
-                
-if __name__ == '__main__':
+
+def kooking_service(out_file, offset = 0):
     while True:
         try:
-            data = kooking_purchases(get_purchases())
+            data = kooking_purchases(get_purchases(offset = offset))
             print(time.asctime(), hashlib.sha1(access_token.encode('utf-8')).hexdigest(), 'purchases:', len(data))
             with open(out_file + '.tmp', 'w') as fd:
                 fd.write(json.dumps(data))
@@ -51,3 +68,6 @@ if __name__ == '__main__':
         except Exception as err:
             print(time.asctime(), repr(err))
         time.sleep(5)
+
+if __name__ == '__main__':
+    kooking_service(out_file)
